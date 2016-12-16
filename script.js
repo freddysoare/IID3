a6a573b3ec686cdb728921a27410e35fbf679932
Momo <e1425603@student.tuwien.ac.at>
2016-12-16 21:09:37 +0100

    Suchfunktion fuer beide Formulare Angebote und Anzeigen bereitgestellt

--- script.js
+++ script.js
@@ -41,3 +41,13 @@ function setCategories() {
 		}
 	}
 }
+
+$("#searchbutton").click(function() {
+	if($("#searchbox").val().length === 0) {
+		$("h2").each(function() {
+			$(this).parent().parent().parent().show();
+		});
+	} else {
+		$("h2:not(:contains('" + $("#searchbox").val() + "'))").parent().parent().parent().hide();
+	}
+});
--- scripts.js
+++
@@ -1,9 +0,0 @@
-$("#searchbutton").click(function() {
-	if($("#searchbox").val().length === 0) {
-		$("h2").each(function() {
-			$(this).parent().parent().parent().show();
-		});
-	} else {
-		$("h2:not(:contains('" + $("#searchbox").val() + "'))").parent().parent().parent().hide();
-	}
-});
